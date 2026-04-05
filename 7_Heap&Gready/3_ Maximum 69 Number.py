e08e1667023a93bc8e9e47fc778be512f589ccd6
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-04-05 23:20:00 +0500

    Solved Maximum 69 Number

---
+++ 7_Heap&Gready/3_ Maximum 69 Number.py
@@ -0,0 +1,32 @@
+## Maximum 69 Number
+
+"""
+You are given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9 or 9 becomes 6).
+
+Input
+A single line containing the integer num (1 ≤ num ≤ 10^4, consisting only of digits 6 and 9).
+
+Output
+Print the maximum number achievable by changing at most one digit.
+
+Sample Input 1:
+9669
+Sample Output 1:
+9969
+Changing the first 6 (second digit) to 9 gives 9969, which is the maximum. Changing the other 6 would give 9699, which is smaller.
+"""
+
+n = int(input())
+dev = 10000
+lis = []
+lis.append(n)
+for i in range(4):
+    dev /= 10
+    res = (n // dev) % 10 
+    if res == 6:
+        final_res = int(n+(dev*3))
+        lis.append(final_res)
+        break
+print(max(lis))
