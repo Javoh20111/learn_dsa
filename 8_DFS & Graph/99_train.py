abc881af53492f87fa6661ce50c044e154ddad73
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-04-06 23:28:12 +0500

    Started trainning

--- 8_DFS & Graph/99_train.py
+++ 8_DFS & Graph/99_train.py
@@ -5,13 +5,20 @@ def dfs(start, graph):
         print(f'Visiting node:{node}')
         visited.add(node)
 
-        for neigbor in graph(node):
-            
+        for neigbor in graph[node]:
+            if neigbor not in visited:
+                explore(neigbor)
+
+    explore(start)
+    return visited
+
 
 
 graph = {
-    0:[1],
-    1:[0,2],
-    2:[1]
+    0: [1, 3],
+    1: [0, 2],
+    2: [1, 3],
+    3: [0, 2]
 }
-print(f'nodes visited: {''}')
+
+print(f'nodes visited: {dfs(0, graph)}')
