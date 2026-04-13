191eb5e965d839e8f5a67251e4eaddb53e301d8e
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-04-13 14:39:10 +0500

    Connecting Cities With Minimum Cost

--- 10_graph/2_Network Setup.py
+++ 10_graph/2_Network Setup.py
@@ -59,4 +59,45 @@ Offices {1,2} and {3,4} are in separate groups. It is impossible to connect all 
 import sys
 input = sys.stdin.readline
 
-def 
+def solve():
+    ## Take input
+    n,m = map(int,input().split())
+    edges = []
+
+    for _ in range(m):
+        u,v,w = map(int,input().split())
+        edges.append((w,v,u))
+    edges.sort()
+    ## root finder
+    parent = list(range(n+1))
+    rank = [0] * (n+1)
+    def find(x):
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
+    ## connector
+    def union(x,y):
+        px,py = find(x),find(y)
+        if px == py:
+            return False
+        if rank[px] < rank[py]:
+            px,py=py,px
+        parent[py] = px
+        if rank[px] == rank[py]:
+            rank[px]+=1
+        return True
+
+    ## Step 1 Make a loop to count and check availablity
+    total = 0
+    total_edges = 0
+    for w,u,v in edges:
+        if union(u,v):
+            total+=w
+            total_edges += 1
+        if total_edges == n-1:
+            break
+    if total_edges != n-1:
+        print(-1)
+    else:
+        print(total_edges,total)
+solve()
