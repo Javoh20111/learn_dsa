f7d1853e1a244b39829f67be9a9bee6ee0fe90ef
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-05-28 11:01:22 +0500

    Implement checkout and receipt generation features with discounts

--- 100_learn_dictionary.py
+++ 100_learn_dictionary.py
@@ -72,7 +72,8 @@ for w in sorted(freq.keys()):
 top = max(freq, key=freq.get)
 print("Most frequent:", top, freq[top]) """
 
-
+from datetime import datetime
+import random
 
 MENU = {
  "espresso": 2.00,
@@ -87,7 +88,36 @@ def is_item_available(name):
         return True
     else:
         return False
-    
+
+
+
+def checkout(orders):
+    subtotal = 0
+    grand_total = 0
+
+
+    now = datetime.now().time()
+
+    start_time = datetime.strptime("15:00", "%H:%M").time()
+    end_time = datetime.strptime("17:00", "%H:%M").time()
+
+    for order in orders:
+            subtotal += order['line_total']
+
+    if start_time <= now <= end_time:
+        for order in orders:
+            if order['name'] in ["espresso", "latte", "tea"]:
+                grand_total += (order['line_total']*0.9)
+            else:
+                grand_total += order['line_total']
+    else:
+        grand_total = subtotal
+
+    return {"subtotal": subtotal, 
+            "discount": round(subtotal - grand_total, 2), 
+            "grand_total": grand_total}
+
+
 def build_order():
     orders = []
     for pro,price in MENU.items():
@@ -104,14 +134,36 @@ def build_order():
                     print("Please enter a number")
             orders.append({
                             "name": order,
-                           "quantity": quantity,
-                           "unit_price": MENU[order],
-                           "line_total":quantity*MENU[order]
+                            "quantity": quantity,
+                            "unit_price": MENU[order],
+                            "line_total":quantity*MENU[order]
                            })
         else:
             print("Item not on menu")
         order = input("What do you order or (done to finish): ").lower()
     return orders
 
-def checkout(order):
+
+def generate_receipt(orders, totals):
+    random.seed(41)
+    receipt_number = random.randint(10000, 99999)
+    now = datetime.now()
+    string_date = now.strftime("%Y-%m-%d %H:%M")
+    print("\n")
+    print(f"# {receipt_number} {string_date}")
+    print("\n"+30*"-")
+    print(f"{'item':<7}{'Qty':>5}{'Unit':>5}{'Line':>5}")
+    for order in orders:
+        print(f"{order['name']:<7}{order['quantity']:>5}{order['unit_price']:>5}{order['line_total']:>5}")
+    print("\n"+30*"-")
+    print(f"{'subtotal':<15}{totals['subtotal']:>5}")
+    print(f"{'subtotal':<15}{totals['discount']:>5}")
+    print(f"{'subtotal':<15}{totals['grand_total']:>5}")
+
+order = build_order()
+
+def save_receipt(text):
     
+
+
+generate_receipt(order , checkout(order))
