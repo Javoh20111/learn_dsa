a6ba5a0bc44379edaad15766ad3865c7d25e85d4
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-05-29 15:50:43 +0500

    working on opening files

--- 101_files.py
+++ 101_files.py
@@ -1,16 +1,42 @@
 """ file_name = "friends.txt"
+ """
+""" with open(file_name, "r", encoding="utf-8") as f:
+    pos0 = f.tell()
+    head = f.read(5)
+    pos1 = f.tell()
+    print(pos1) """
 
-with open(file_name, "w", encoding="utf-8") as f:
-    f.write("Javohir\n")
-    f.write("Eshonov\n") """
 
-
-source = input("Please provide source: ")
+""" source = input("Please provide source: ")
 destination = input("Please provide destination: ")
 
+try:
+    with open(source, "r", encoding="utf-8") as f:
+        data = f.read()
+        with open(destination, "w", encoding="utf-8") as d:
+            d.write(data)
+except:
+    print("File not found! Please check out sourse location!") """
 
-with open(source, "r", encoding="utf-8") as f:
-    data = f.read()
+""" file_name = "contacts.txt"
+try:
+    with open(file_name, "r", encoding="utf-8") as f:
+        line = f.readline()
+        while line != "":
+            print(line, end="")
+            line = f.readline()
+except FileNotFoundError:
+    print("Error: file not found.") """
 
-with open(destination, "w", encoding="utf-8") as d:
-    d.write(data)
+from datetime import datetime
+import pandas as pd 
+
+""" with open("person.csv", "a+", encoding="utf-8") as l:
+    name = input("Enter your name")
+    time = datetime.now().strftime("%Y-%m-%d %H-%M")
+    l.write(f"\n{name},{time}") """
+
+
+""" df = pd.read_csv("person.csv", header=None, names = ["name", "date"])
+print(df) """
+
