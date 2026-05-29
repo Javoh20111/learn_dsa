b036abf69fa6a08400764a771946c42b32eac0fc
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-05-29 18:02:46 +0500

    learning csv module

--- 101_files.py
+++ 101_files.py
@@ -40,3 +40,32 @@ import pandas as pd
 """ df = pd.read_csv("person.csv", header=None, names = ["name", "date"])
 print(df) """
 
+""" with open("contacts.txt", "r", encoding="utf-8") as c:
+    rows = []
+    for line in c:
+        line = line.strip("\n")
+        if line =="":
+            continue
+        rows.append(line)
+count = 0 
+total = 0
+for elem in rows:
+    count+=1
+    elem = elem.split(",")
+    name = elem[0].strip()
+    age = int(elem[1].strip())
+    total+=age
+ """
+
+import csv
+
+""" people = [
+ ["name", "age"],
+ ["Mara Pinto", 25],
+ ["Rúben Pinto", 18]
+]
+
+with open("people.csv", "w",  newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerows(people) """
+
